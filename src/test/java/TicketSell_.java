354822981213960682289903d30fe1b67e110a91
ismael094 <ismaelpitufo@gmail.com>
2019-10-13 15:13:36 +0100

    Fifth test

--- src/test/java/TicketSell_.java
+++ src/test/java/TicketSell_.java
@@ -8,8 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TicketSell_ {
 
+    public static final String NO = "NO";
+    public static final String YES = "YES";
+    public static final int DOLLARS_25 = 0;
+    public static final int DOLLARS_50 = 1;
     private final String value;
     private final int[] number;
+    private int[] billets = new int[3];
 
     public TicketSell_(String value, int[] number) {
         this.value = value;
@@ -19,10 +24,11 @@ public class TicketSell_ {
     @Parameterized.Parameters
     public static Object[][] cases() {
         return new Object[][] {
-                {"NO", new int[]{}},
-                {"YES", new int[]{25}},
-                {"NO", new int[]{50}},
-                {"YES", new int[]{25,25}}
+                {NO, new int[]{}},
+                {YES, new int[]{25}},
+                {NO, new int[]{50}},
+                {YES, new int[]{25,25}},
+                {YES, new int[]{25,50}}
         };
     }
 
@@ -32,10 +38,29 @@ public class TicketSell_ {
     }
 
     private String sellTicketFor(int[] number) {
-        if (number.length == 0) return "NO";
+        if (number.length == DOLLARS_25) return NO;
+
         for (int dollars : number)
-            if (number[0] != 25) return "NO";
-        return "YES";
+            if (dollars == 25)
+                sum25Dollars();
+            else if (dollars == 50 && num25Dollars()> DOLLARS_25)
+                sum50Dollars();
+            else
+                return NO;
+        return YES;
     }
+
+    private void sum50Dollars() {
+        billets[DOLLARS_50]++;
+    }
+
+    private int num25Dollars() {
+        return billets[DOLLARS_25];
+    }
+
+    private void sum25Dollars() {
+        billets[DOLLARS_25]++;
+    }
+
 
 }
